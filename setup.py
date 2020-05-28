d5b31aa0a027536f930b94b87519dd4c1d67d37e
Deepak Seshadri <deepak.seshadri.linux@gmail.com>
2020-05-28 17:15:04 -0500

    updated setup.py with python binary version requirement and find_packages()

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def readme():
@@ -11,9 +11,10 @@ setup(
     version='0.1',
     description='Download FINVIZ US Equities data by scraping the HTML tables on the site',
     long_description=readme(),
-    packages=['download_finviz_data'],
     author='Deepak Seshadri',
+    packages=find_packages(),
     license='MIT',
+    python_requires='>=3.8',
     entry_points={
         'console_scripts': [
             'download_finviz_data = download_finviz_data.__main__:main'
